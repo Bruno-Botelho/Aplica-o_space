88c6a8898d160ec52be7a119c521d9c93c84f080
Bruno <bruno.botelho@live.com>
2024-01-06 19:04:01 -0300

    teste

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index,imagem
 
 # POR BOA PRATICA CRIAMOS UM ARQUIVO DENTRO DO APP QUE IRÁ CONTER TODAS AS ROTAS
 # E DEPOIS NAS ROTAS PRINCIPAIS IREMOS CHAMA-LO
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
